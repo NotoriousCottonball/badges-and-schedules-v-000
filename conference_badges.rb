21f39a7008be3bad4c4db1706243dcc461e77643
George  Pianka <gpianka1@gmail.com>
2018-04-21 11:47:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,6 @@ def batch_badge_creator(array_of_names)
 end
  
 def assign_rooms(array_of_names) 
-  array_of_names.collect.with_index {|x,i| ""
+  array_of_names.collect.with_index {|x,i| "Hello #{x}"
 
   "Hello, #{i}! You'll be assigned to room 
